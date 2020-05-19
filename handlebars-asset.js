e98387ec6bb476330484ceda76d6d6d0253ed788
Joannic Laborde <joannic@sparkart.com>
2020-05-19 11:59:52 -0300

    module.exports instead of export default (for requires)

--- handlebars-asset.js
+++ handlebars-asset.js
@@ -6,8 +6,7 @@ class HbsAsset extends JSAsset {
         const precompiled = Handlebars.precompile(this.contents);
         this.contents = `
                 import Handlebars from 'handlebars/dist/handlebars.runtime';
-                const templateFunction = Handlebars.template(${precompiled});
-                export default templateFunction;
+                module.exports = Handlebars.template(${precompiled});
                 `;
 
         return await super.pretransform();
